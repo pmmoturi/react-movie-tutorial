32901c19f747c6ccac6c5b02015e0449c268179c
Philip Moturi <pmmoturi@gmail.com>
2025-02-28 17:17:31 +0300

    Fetching popular movies via API working well. Searching via API is still pending

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,37 +4,31 @@ import "../css/Home.css";
 import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
-  const movies = [
-    {
-      id: 1,
-      title: "The Godfather",
-      release_date: "March 24, 1972",
-      poster:
-        "https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/films/the-godfather.jpg?itok=4v9Q5v8w",
-    },
-    {
-      id: 2,
-      title: "The Shawshank Redemption",
-      release_date: "September 23, 1994",
-      poster:
-        "https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/films/the-shawshank-redemption.jpg?itok=4v9Q5v8w",
-    },
-    {
-      id: 3,
-      title: "The Dark Knight",
-      release_date: "July 18, 2008",
-      poster:
-        "https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/films/the-dark-knight.jpg?itok=4v9Q5v8w",
-    },
-  ];
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (error) {
+        console.error("Error loading popular movies", error);
+        setError("Error loading popular movies");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
     alert(searchQuery);
     console.log("Search button clicked");
   };
-
-  const [searchQuery, setSearchQuery] = useState("");
 
   return (
     <div className="home">
@@ -49,11 +43,18 @@ function Home() {
           Search
         </button>
       </form>
-      <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
-      </div>
+
+      {error && <div className="error">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
